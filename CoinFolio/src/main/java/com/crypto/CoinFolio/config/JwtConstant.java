1797cd8ac38afd578fd6458678f8a9fed839f783
Aman Sharma <amansharmaofficial0115@gmail.com>
2024-10-15 01:34:45 +0530

    Refactored hardcoded SECRET_KEY

--- CoinFolio/src/main/java/com/crypto/CoinFolio/config/JwtConstant.java
+++ CoinFolio/src/main/java/com/crypto/CoinFolio/config/JwtConstant.java
@@ -1,7 +1,9 @@
 package com.crypto.CoinFolio.config;
 
+import com.crypto.CoinFolio.constants.constants;
+
 public class JwtConstant {
-    public static final String SECRET_KEY = "asfnbpngonpgowengponerbhnpoiohfqiwfnbvooiewbgvip0o2b";
+    public static final String SECRET_KEY = constants.JWT_CONSTANT;
 
     public static final String JWT_HEADER = "Authorization";
 }
